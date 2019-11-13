45f6f5d2c6e1558a20063ce9f3b98db5196242c5
oladimeji-ojo <ojokuredim@gmail.com>
2019-11-13 20:00:16 +0000

    created a deptChecker middleware to only allow BE dept to access users

--- Auth/middlewares/checkDept.js
+++ Auth/middlewares/checkDept.js
@@ -1,3 +1,9 @@
-// module.exports = (req, res, next ) => {
-//     return 
-// }
+module.exports = role => (req, res, next) => {
+  if (role === req.user.dept) {
+    next();
+  } else {
+    res.status(403).json({
+      message: "only BE developers can access this information "
+    });
+  }
+};
--- Users/user-router.js
+++ Users/user-router.js
@@ -2,11 +2,15 @@ const usersRouter = require("express").Router();
 
 const Users = require("./users-model.js");
 const restricted = require("../Auth/middlewares/restricted");
+const checkDept = require("../Auth/middlewares/checkDept");
 
-usersRouter.get("/", restricted, (req, res) => {
+usersRouter.get("/", restricted, checkDept('BE'), (req, res) => {
   Users.find()
     .then(users => {
-      res.status(200).json(users);
+      res.status(200).json({
+        loggedInUser: req.username,
+        users
+      });
     })
     .catch(err => {
       res.status(500).json({
